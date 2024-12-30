ec38ef9a70923a6d9dd5e6ff0da5afebda4f83ba
Tehan1234 <anupatehan@gmail.com>
2024-12-30 18:40:19 +0530

    make website responsive

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ function opentab(tabname) {
   event.currentTarget.classList.add("active-link")
   document.getElementById(tabname).classList.add("active-tab");
 }
+
+var sidemenu = document.getElementById("slidemenu");
+
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
